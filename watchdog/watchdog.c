93c3bf62d597b40db0eb59f11fbfdc0668235ded
Steve Chen <steve.chen@i2som.tech>
2020-03-25 16:33:19 +0800

    watchdog: add parse options

--- watchdog/watchdog.c
+++ watchdog/watchdog.c
@@ -14,69 +14,129 @@
 #include <sys/signal.h>
 #include <linux/watchdog.h>
 
-//watchdog
-#define WATCHDOG_IOCTL_BASE     'W'
-
-int wdt_status= 1;
-int wdt_fd;
 int time_out = 5;
+static const char *device = "/dev/watchdog";
+static int enable = 1;
+static int keepalive = 20;
 
 int main(int argc, char *argv[])
 {
 	int new_time;
-	int i;
-	int ret, count = 20;
+	int i, ret;
 	struct watchdog_info wdt_info;
+	int wdt_fd;
+	int opts = 0;
 
-	wdt_fd = open("/dev/watchdog0", O_RDWR);
-	if(wdt_fd == -1)
-		perror("Open Watchdog ERROR!\n");
+	parse_opts(argc, argv);
+	wdt_fd = open(device, O_RDWR);
+	if(wdt_fd < 0) {
+		perror("Open Watchdog failed!\n");
+		return -1;
+	};
 
-	//get watchdog infomation struct
-	ioctl(wdt_fd, WDIOC_GETSUPPORT, &wdt_info);
-	printf("options=%d, identify=%s\n", wdt_info.options, wdt_info.identity);
-
-	//set timeout
-	ioctl(wdt_fd, WDIOC_SETTIMEOUT, &time_out);
-
-	//read the timeout value
-	ioctl(wdt_fd, WDIOC_GETTIMEOUT, &time_out);
-
-	new_time = time_out;
-	printf("time_value=%d\n", new_time);
-#if 0
-	//disable the watchdog
-	i=WDIOS_DISABLECARD;
-	printf("%d\n",ioctl(wdt_fd,WDIOC_SETOPTIONS,&i));
-
-	//enable the watchdog
-	i=WDIOS_ENABLECARD;
-	printf("%d\n",ioctl(wdt_fd,WDIOC_SETOPTIONS,&i));
-#endif
-	while(1)
-	{
-		count--;
-
-		//feed time
-		if(wdt_status == 1)
-		{
-			ioctl(wdt_fd,WDIOC_KEEPALIVE,NULL);
-			//write(wdt_fd,NULL,1);
-			//feed time, too
-			printf("Feed time!\n");
-
+	if(enable == 0) {
+		// needs write 'V' before disable the watchdog, it means WDT_OK_TO_CLOSE
+		if (write(wdt_fd, "V", 1) != 1) {
+			printf("write WDT_OK_TO_CLOSE failed!");
 		}
+		opts = WDIOS_DISABLECARD;
+		ret = ioctl(wdt_fd, WDIOC_SETOPTIONS, &opts);
+		printf("watchdog disable!\r\n");
 
-		sleep(1);
+	} else {
+#if 0
+		//enable the watchdog
+		i=WDIOS_ENABLECARD;
+		printf("%d\n",ioctl(wdt_fd,WDIOC_SETOPTIONS,&i));
+#endif
 
-		if(count == 0)
-		{
-			wdt_status = 0;
-			printf("End of feeding time!\n");
+		//get watchdog infomation struct
+		ioctl(wdt_fd, WDIOC_GETSUPPORT, &wdt_info);
+		printf("options=%d, identify=%s\n", wdt_info.options, wdt_info.identity);
+
+		//set timeout
+		ioctl(wdt_fd, WDIOC_SETTIMEOUT, &time_out);
+
+		//read the timeout value
+		ioctl(wdt_fd, WDIOC_GETTIMEOUT, &time_out);
+
+		new_time = time_out;
+		printf("time_value=%d\n", new_time);
+
+		if(keepalive > 0) {
+			for(i = 0; i < keepalive; i++) {
+				ret = ioctl(wdt_fd, WDIOC_KEEPALIVE, NULL);
+				printf("Feed the watchdog! keepalive = %d, ret = %d\n", i + 1, ret);
+				sleep(1);
+			}
+		}
+	};
+
+	ret = close(wdt_fd);
+	if(ret != 0) {
+		printf("the watchdog device close failed, watchdog will keep on!\r\n");
+	};
+
+	return ret;
+}
+
+void print_usage(const char *prog)
+{
+	printf("Usage: %s [-desk]\r\n", prog);
+	puts("  -d --device       device to use (default /dev/watchdog0)\r\n"
+	     "  -e --enable       0 = disable, 1 = enable(default)\r\n"
+	     "  -s --timeout      seconds for timeout setting, 1~4194304(default 20)\r\n"
+	     "  -k --keepalive    seconds for keep alive test show, 0~4194304(default 5)\r\n");
+	exit(1);
+}
+
+void parse_opts(int argc, char *argv[])
+{
+	int c;
+	static const struct option lopts[] = {
+		{"device",    1, 0, 'd'},
+		{"enable",    1, 0, 'e'},
+		{"timeout",   1, 0, 's'},
+		{"keepalive", 1, 0, 'k'},
+		{NULL, 0, 0, 0},
+	};
+
+
+	while (1) {
+		c = getopt_long(argc, argv, "d:e:s:k:", lopts, NULL);
+
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case 'd':
+			device = optarg;
+			break;
+		case 'e':
+			enable = atoi(optarg);
+			if ((enable < 0) || (enable > 1)) {
+				printf("enable parameter error,it must be 0 or 1\r\n");
+				exit(1);
+			}
+			break;
+		case 's':
+			time_out = atoi(optarg);
+		        if ((time_out <= 0) || (time_out > (0x10000000 >> 6))) {
+				printf("timeout parameter error,it must be 1 to 4194304\r\n");
+				exit(1);
+			}
+			break;
+	        case 'k':
+			keepalive = atoi(optarg);
+			if ((keepalive < 0) || (keepalive> (0x10000000 >> 6))) {
+				printf("keepalive parameter error,it must be 0 to 4194304\r\n");
+				exit(1);
+			}
+			break;
+		default:
+			print_usage(argv[0]);
+			break;
 		}
 	}
-
-	close(wdt_fd);
-	return 0;
 }
 
